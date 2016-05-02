d5c130c58f07fddffd8a94bef7fd9bd55c17708b
Roger Beaman <rwbeaman@gmail.com>
2016-05-01 21:05:02 -0400

    rewrote in ES5

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-'use strict';
+var dupeSlash = /(\/+)/g;
+var moreThanThreePeriods = /\.{3,}/g;
 
-const dupeSlash = /(\/+)/g;
+// polyfill Array.isArray if necessary
+if (!Array.isArray) {
+  Array.isArray = function(arg) {
+    return Object.prototype.toString.call(arg) === '[object Array]';
+  };
+}
 
 // works just like Node.js's native path library's 'join' function with the bonus of handling arrays
-export default function pathJoin(...args) {
-  return args.map(val => {
-    return val = typeof val === 'string' ? val : // if string just keep as is
-      Array.isArray(val) ? pathJoin(...val) : // handle array with recursion
-      console.error("tried to join something other than a string or array, it was ignored in pathJoin's result"),
-      val && val || ''; // add back slash at the beginning
-  }).join('/').replace(dupeSlash, '/'); // join the resulting array together
+export default function pathJoin() {
+  return Array.prototype.slice.call(arguments).map(function(val) {
+    return val = typeof val === 'string' ? "" + val : // if string or number just keep as is
+      Array.isArray(val) ? pathJoin.apply(null, val) : // handle array with recursion
+      (console.error || console.log)('tried to join something other than a string or array, it was ignored in pathJoin\'s result'),
+      val && val || '';
+  }).join('/').replace(dupeSlash, '/').replace(moreThanThreePeriods, '..'); // join the resulting array together
 }
+
+module.exports = pathJoin;
